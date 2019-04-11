35545d986280b560ee84b35c8173c1d4a061ae05
Spencer Shaw <sfshaw@calpoly.edu>
2019-04-11 09:45:14 -0700

    Utilize LCD header file

--- lcd.c
+++ lcd.c
@@ -2,32 +2,9 @@
 #include "lcd.h"
 #include "delay.h"
 
-#define RS  0b00000001  // P4.0
-#define RW  0b00000010  // P4.1
-#define EN  0b00000100  // P4.2
-#define DB0 0b00001000  // P4.3
-#define DB1 0b00010000  // P4.4
-#define DB2 0b00100000  // P4.5
-#define DB3 0b01000000  // P4.6
-#define LCD_PINS_MASK (DB3 | BD2 | DB1 | DB0 | EN | RW | RS)  // P4.0-4.6
-
-#define CMD_CLR_DISP 0x01
-#define CMD_HOME 0x02  //  Send cursor to home position
-//  lcd Mode options
-#define CMD_MODE_8_BIT 0x30  // 0b00110000
-#define CMD_MODE_4_BIT 
-#define CMD_MODE_2_LINE 0x28
-#define CMD_CURSOR_ON 0x0A
-#define CMD_CURSOR_BLINK 0x09
-#define CMD_DISPLAY_ON 0x0C
-#define CMD_CURSOR_RIGHT 0x06  //  Cursor moves to right
-#define CMD_CURSOR_LEFT 0x04  //  Cursor moves to left
-#define CMD_SET_CURSOR 0x80  //  Set cursor position to SET_CURSOR ADDRESS
-
 
 //  8-bit mode, 2 line, cursor on
-void lcd_init()
-{
+void lcd_init(){
     P4->SEL0 &= ~LCD_PINS_MASK;  // Initialize all P4 pins
     P4->SEL1 &= ~LCD_PINS_MASK;  // as GPIO 
     P4->DIR |= LCD_PINS_MASK;  // Set pins to output mode
@@ -53,8 +30,7 @@ void lcd_init()
     lcd_command(CMD_CURSOR_RIGHT);    // Entry Mode set
 }
 
-void lcd_command(char i)
-{
+void lcd_command(char i){
     P1 = i; // put data on output Port
     D_I =0; // D/I=LOW : send instruction
     R_W =0; // R/W=LOW : Write
@@ -64,8 +40,7 @@ void lcd_command(char i)
     Nybble(); // Send upper 4 bits
 }
 
-void lcd_write(char i)
-{
+void lcd_write(char i){
     P1 = i; // put data on output Port
     D_I =1; // D/I=HIGH : send data
     R_W =0; // R/W=LOW : Write
@@ -75,14 +50,13 @@ void lcd_write(char i)
     Nybble(); // Clock upper 4 bits
 }
 
-static void Nybble()
-{
+static void Nybble(){
     E = 1;
     NOP //Delay(1);    // enable pulse width >= 300ns
     E = 0;    // Clock enable: falling edge
 }
 
-void lcd_init_8bit(void) {
+void lcd_init_8bit(void){
     //  Setup GPIO for P3 and P4 to use lcd
     // P3->SEL0 &= ~(RS | RW | EN);
     P3->SEL1 &= ~(RS | RW | EN);
--- lcd.h
+++ lcd.h
@@ -1,15 +1,35 @@
-/*
- * lcd.h
- *
- *  Created on: Apr 9, 2019
- *      Author: sfshaw
- */
-
 #ifndef LCD_H_
 #define LCD_H_
 
 
+#define RS  0b00000001  // P4.0
+#define RW  0b00000010  // P4.1
+#define EN  0b00000100  // P4.2
+#define DB0 0b00001000  // P4.3
+#define DB1 0b00010000  // P4.4
+#define DB2 0b00100000  // P4.5
+#define DB3 0b01000000  // P4.6
+#define LCD_PINS_MASK (DB3 | BD2 | DB1 | DB0 | EN | RW | RS)  // P4.0-4.6
 
+#define CMD_CLR_DISP 0x01
+#define CMD_HOME 0x02  //  Send cursor to home position
+//  lcd Mode options
+#define CMD_MODE_8_BIT 0x30  // 0b00110000
+#define CMD_MODE_4_BIT 
+#define CMD_MODE_2_LINE 0x28
+#define CMD_CURSOR_ON 0x0A
+#define CMD_CURSOR_BLINK 0x09
+#define CMD_DISPLAY_ON 0x0C
+#define CMD_CURSOR_RIGHT 0x06  //  Cursor moves to right
+#define CMD_CURSOR_LEFT 0x04  //  Cursor moves to left
+#define CMD_SET_CURSOR 0x80  //  Set cursor position to SET_CURSOR ADDRESS
+
+void lcd_init();
+void lcd_command(char i);
+void lcd_write(char i);
+static void Nybble();
+
+void lcd_init_8bit(void); // TODO: remove this
 
 
 #endif /* LCD_H_ */
